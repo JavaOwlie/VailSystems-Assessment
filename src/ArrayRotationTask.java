919534018a762f6ea93e249c4a338f84e924b784
JavaOwlie <alenadarazhei@yahoo.com>
2023-02-03 19:30:03 -0600

    Negative  values  for  the  positions  to  rotate  will  throw  an  error

--- src/ArrayRotationTask.java
+++ src/ArrayRotationTask.java
@@ -7,9 +7,17 @@ public class ArrayRotationTask {
     public static void main(String[] args) {
         int [] givenArray = {1,2,3,4,5,6,7};
 
+        /*
         System.out.println(Arrays.toString(rotateArrayLeft(givenArray, 2)));
         System.out.println(Arrays.toString(rotateArrayLeft(givenArray, 8)));
         System.out.println(Arrays.toString(rotateArrayLeft(givenArray, 100)));
+        System.out.println(Arrays.toString(rotateArrayLeft(givenArray, -2)));
+
+         */
+
+        givenArray[2] = -3;
+
+        System.out.println(Arrays.toString(rotateArrayLeft(givenArray, 2)));
 
     }
 
@@ -19,11 +27,30 @@ public class ArrayRotationTask {
 
     public static  int [] rotateArrayLeft (int [] givenArray, int numberOfPositions){
 
+
         while (numberOfPositions > givenArray.length) {
             numberOfPositions -= givenArray.length;
         }  // handles a position value greater than the number of integers in the array
 
         int[] resultArray = new int[givenArray.length];
+
+        try {
+
+            if (numberOfPositions < 0) {
+                throw new NegativeNumberException();
+            }
+
+            for (int each : givenArray) {
+                if (each < 0) {
+                    throw new NegativeNumberException();
+                }
+            }
+
+        } catch (NegativeNumberException e){
+            e.printStackTrace();
+            return resultArray; // will return array of zeroes
+        }
+
 
 
         int givenIndex = numberOfPositions;
---
+++ src/NegativeNumberException.java
@@ -0,0 +1,11 @@
+public class NegativeNumberException extends Exception{
+
+
+    public NegativeNumberException(){
+        super("Negative numbers are not allowed");
+    }
+
+
+
+
+}
