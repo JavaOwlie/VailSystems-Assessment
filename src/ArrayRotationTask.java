09b78e2a9f933cd39e34dac21058b8fe4ff747f2
JavaOwlie <alenadarazhei@yahoo.com>
2023-02-03 19:16:39 -0600

    Base is ready

--- src/ArrayRotationTask.java
+++ src/ArrayRotationTask.java
@@ -1,6 +1,44 @@
+import java.util.Arrays;
+
 public class ArrayRotationTask {
 
 
+
+    public static void main(String[] args) {
+        int [] givenArray = {1,2,3,4,5,6,7};
+
+        System.out.println(Arrays.toString(rotateArrayLeft(givenArray, 2)));
+
+    }
+
+
+
+
+
+    public static  int [] rotateArrayLeft (int [] givenArray, int numberOfPositions){
+
+        int[] resultArray = new int[givenArray.length];
+
+
+        int givenIndex = numberOfPositions;
+
+
+        for (int i = 0; i < resultArray.length - numberOfPositions; i++) {
+            resultArray[i] = givenArray[givenIndex];
+            givenIndex++;
+        }
+
+        givenIndex = 0;
+
+        for (int i = resultArray.length - numberOfPositions; i < resultArray.length; i++) {
+            resultArray[i] = givenArray[givenIndex];
+            givenIndex++;
+        }
+
+
+        return resultArray;
+
+    }
 
 
 }
